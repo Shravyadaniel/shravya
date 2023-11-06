37a75ce5d5119ecbf1ce037351d36671f52a2045
Shravyadaniel <putla.shravya@gmail.com>
2023-11-06 13:26:45 +0530

    unary operator

---
+++ .vscode/16.c
@@ -0,0 +1,11 @@
+//unary minus operator
+#include <stdio.h>
+int main()
+{
+int positivenumber;https://github.com/Shravyadaniel/shravya
+printf("enter the positive number:\n");
+scanf("%d",&positivenumber);
+int negativenumber = -positivenumber;
+printf("the negative number is %d", negativenumber);
+return 0;
+}
---
+++ .vscode/tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+//unary operator
+#include <stdio.h>
+int main()
+{
+int positivenumber;
+int positivenumber = -positivenumber;
+printf("enter the positive number:\n");
+scanf("%d",&positivenumber);
+printf("the negative number is %d", positivenumber);
+return 0;
+}
