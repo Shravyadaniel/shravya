e8b7ce47f027f8d5eb577bc4417fe00a75c6522e
Shravyadaniel <putla.shravya@gmail.com>
2023-11-14 15:18:47 +0530

    Arrays

--- .vscode/27.c
+++ .vscode/27.c
@@ -1,6 +1,6 @@
-/*
+/*/*
 An array is a container object that holds a fixed number of values of a single type. \
-The length of an array is established when the array is created. 
+The length of an array is established when the array is created.
 After creation, its length is fixed.
 Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
 
@@ -12,11 +12,12 @@ To access an array element, refer to its index number.
 Array indexes start with 0: [0] is the first element. [1] is the second element, etc.
 
 */
-//Program to print random number in Arrays
+// Program to print random number in an Array
 #include <stdio.h>
 int main()
 {
-    int num[6]={3,7,11,15,19,26};
-    printf("the output will be %d",num[3]);
+    int num[6] = {3, 7, 11, 15, 19, 26};
+    num[1] = 13;
+    printf("the output will be %d", num[1]);
     return 0;
 }
