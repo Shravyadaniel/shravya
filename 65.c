45e8a2607202742d9f7fd480cb7d4c3dd8e95f93
Shravyadaniel <putla.shravya@gmail.com>
2023-12-07 15:31:14 +0530

    while loop

---
+++ 65.c
@@ -0,0 +1,12 @@
+//count down program
+#include <stdio.h>
+int main()
+{
+    int countdown=7;
+    while (countdown>0)
+    {
+        printf("%d \n",countdown);
+        countdown--;
+    }
+    printf("MERRY CHRISTMAS!!");
+}
