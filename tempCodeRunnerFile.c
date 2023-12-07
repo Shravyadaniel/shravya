ee1579c512e68c31e91c6ba8fbdc403844b9a01f
Shravyadaniel <putla.shravya@gmail.com>
2023-12-07 14:46:17 +0530

    while loop

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,13 +1,16 @@
-//program to print 10 multiples when certain number is given pre defined
-//using while
+//program to print the string in the reverse order
+//using while loop
 #include <stdio.h>
 int main()
 {
-    int i=0;
-    while(i<100)
+    int i;
+    printf("enter the number: ");
+    scanf("%d",&i);
+    while(i>=0)
     {
         printf("%d ",i);
-        i=i+10;
+        i--;
     }
     return 0;
 }
+    
